e05b71fc0051c9c560bb323f4d1b70fd5282a016
unknown <thanhtuan27092000@gmail.com>
2023-06-06 10:02:39 +0700

    Top-Simulation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import IsamiGiken from './components/IsamiGiken';
-import Fotter from './components/Footer'
+import Footer from './components/Footer'
+import TopSimulation from './components/TopSimulation'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <IsamiGiken/>
-      <Fotter></Fotter>
+      <TopSimulation/>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/TopSimulation/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+const Simulation = () => {
+  return (
+    <div className="top-simulation">
+      <img className="top-simulation-background"
+        src="	https://isamigiken.gattscom.com/image/top/simulation_bg.jpg"
+        alt=""
+      ></img>
+      <a href="https://home.gattscom.com/">
+        <img
+          src="https://isamigiken.gattscom.com/image/top/simulation_mobile.png?_20230309"
+          alt=""
+        ></img>
+      </a>
+    </div>
+  );
+};
+
+export default Simulation;
